586b9369307ea7805816af7e6050737e9b735e55
Diya Chauhan <diyachauha1234@gmail.com>
2023-10-19 08:13:52 +0530

    pr9

--- app/src/main/java/com/diyachauhan/mad_practical_9_21012011011/activity_splash.kt
+++ app/src/main/java/com/diyachauhan/mad_practical_9_21012011011/activity_splash.kt
@@ -2,10 +2,12 @@ package com.diyachauhan.mad_practical_9_21012011011
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class activity_splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
     }
 }
